d69309527873e7fdf60ed5af1821c8934f2095e3
zhangcheng <huangbaoying@oasgames.com>
2021-04-09 18:06:15 +0800

    *fix bug

--- 2020/bookSpider/test_book.py
+++ 2020/bookSpider/test_book.py
@@ -5,22 +5,22 @@ from bookSite import BookSite
 from BookPathGuess import BookPathGuess
 
 if __name__ == '__main__':
-    site = BookSite(file="siteConfig/99csw.json", debug=False, download=True)
-    site.scratch_book("毛泽东选集")
+    # site = BookSite(file="siteConfig/99csw.json", debug=False, download=True)
+    # site.scratch_book("毛泽东选集")
 
-    # book_name = "人类简史"
-    # if len(sys.argv) > 1:
-    #     book_name = sys.argv[1]
-    #
-    # dirPath = "siteConfig/"
-    # for f in os.listdir(dirPath):
-    #     siteConfig = os.path.join(dirPath, f)
-    #     site = BookSite(file=siteConfig, download=True, debug=False)
-    #     try:
-    #         site.scratch_book(book_name)
-    #     except:
-    #         traceback.print_exc()
-    #     finally:
-    #         if os.path.exists('{0}.txt'.format(book_name)):
-    #             break
+    book_name = "人类简史"
+    if len(sys.argv) > 1:
+        book_name = sys.argv[1]
+
+    dirPath = "siteConfig/"
+    for f in os.listdir(dirPath):
+        siteConfig = os.path.join(dirPath, f)
+        site = BookSite(file=siteConfig, download=True, debug=False)
+        try:
+            site.scratch_book(book_name)
+        except:
+            traceback.print_exc()
+        finally:
+            if os.path.exists('{0}.txt'.format(book_name)):
+                break
 
